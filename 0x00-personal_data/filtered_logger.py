46b60763749d90931d702d85c066ee6bc52e98aa
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-08 17:01:26 +0100

    0x00-personal_data tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,8 @@ import csv
 import logging
 import os
 import re
-import mysql.connector
 from typing import List
+import mysql.connector
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
