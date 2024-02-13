7a6bfa824b3bf1cfa74753755f65263020060088
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-13 01:26:16 +0100

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Auth Class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Create a class to manage the API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Define require_auth
+        """
+        if path is None:
+            return True
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if path.endswith(excluded_path):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Define authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Define current_user"""
+        return None
