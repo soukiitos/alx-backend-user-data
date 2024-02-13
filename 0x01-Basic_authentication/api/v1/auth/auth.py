39e1fd3338707093c0097d6c11fcaaabf647973b
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-13 22:00:05 +0100

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,15 +12,22 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Define require_auth
         """
-        if path is None:
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if not excluded_paths or len(excluded_paths) == 0:
+        if len(path) == 0:
             return True
-        if not path.endswith('/'):
+        exc_paths = True if path[len(path) - 1] == '/' else False
+        if not exc_paths:
             path += '/'
         for excluded_path in excluded_paths:
-            if path.endswith(excluded_path):
-                return False
+            if len(excluded_path) == 0:
+                continue
+            if excluded_path[len(excluded_path) - 1] != '*':
+                if path == excluded_path:
+                    return False
+            else:
+                if excluded_path[:-1] == path[:len(excluded_path) - 1]:
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
