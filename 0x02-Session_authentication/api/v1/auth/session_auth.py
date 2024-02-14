5bb6549f10cd2b47c0721eee05ca6063fc6c748c
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-02-15 00:24:48 +0100

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Session Authentication Class
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """Class SessionAuth
+    """
+    pass
